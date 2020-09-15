314e998a3923def8a5ad62ee1b8070ce9d908804
luochao <1152597708@qq.com>
2020-09-15 14:45:53 +0800

    创建springcloud-parent测试项目 Signed-off-by: luochao <1152597708@qq.com>

---
+++ eureka_client/src/main/java/com/example/eureka_client/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.eureka_client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("hi")
+    @ResponseBody
+    public String home(@RequestParam String name) {
+        return "端口号:" + port + ", name:" + name;
+    }
+}
